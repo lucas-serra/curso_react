a4297e4d1f4122eedf33680301f3254e38ecf126
lucas-serra <lucasdserra1234@gmail.com>
2023-06-09 17:31:29 -0300

    Adicionando estados.

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Lista.module.scss';
 import Item from './Item';
 
 function Lista () {
 
-    const tarefas = [{tarefa:'React',tempo:"01:00:00"},{tarefa:"JavaScript",tempo:"02:00:00"}];
+    let [tarefas, setTarefas] = useState([{tarefa:'React',tempo:"01:00:00"},{tarefa:"JavaScript",tempo:"02:00:00"}]);
     return(
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTarefas([...tarefas, {tarefa: "Estudar estado", tempo:"05:00:00"}]);
+            }}>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item,index) => (
                     <Item
