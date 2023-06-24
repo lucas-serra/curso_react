5d17b96bc81fb0ee62019b066454bb8284abbf38
lucas-serra <lucasdserra1234@gmail.com>
2023-06-23 22:30:01 -0300

    Exibindo tempo no relógio

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,15 +1,27 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
 import { tempoParaSegundos } from "../../common/utils/time";
 
-export default function Cronometro() {
-    console.log("conversão: ", tempoParaSegundos("01:01:01"));
+interface Props {
+    selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro({ selecionado}: Props) {
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect( () => {
+        if(selecionado?.tempo){
+            setTempo(tempoParaSegundos(selecionado.tempo));
+        }
+    },[selecionado])
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
             <div className={style.relogioWrapper}>
-                <Relogio></Relogio>
+                <Relogio tempo = {tempo}/>
             </div>
             <Botao>Começar</Botao>
         </div>
